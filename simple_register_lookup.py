88a7e4e1ce30b4e4cc68cbaf38196bf583d6d85c
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-01-06 10:36:04 -0700

    Added pydoc strings to simple_register_lookup.py

--- simple_register_lookup.py
+++ simple_register_lookup.py
@@ -1,3 +1,9 @@
+"""Simple Modbus Map register lookup web application.
+
+@author Sam Pottinger
+@license GNU GPL v2
+"""
+
 import json
 import os
 
@@ -8,12 +14,12 @@ import serialize
 
 
 app = flask.Flask(__name__)
-app.debug = True
 modbus_maps = ljmmm.get_device_modbus_maps()
 
 
 @app.route("/")
 def show_ui():
+    """Display the JavaScript client for viewing MODBUS map information."""
     return flask.render_template(
         "simple_register_lookup.html",
         device_names = modbus_maps.keys()
@@ -22,6 +28,7 @@ def show_ui():
 
 @app.route("/lookup/<device_name>.json")
 def lookup_device(device_name):
+    """Render JSON formatted device MODBUS map."""
     modbus_map = modbus_maps.get(device_name, None)
     if modbus_map == None:
         flask.abort(404)
