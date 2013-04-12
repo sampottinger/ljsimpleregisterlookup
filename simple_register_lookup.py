1a00f91c393d10fe361ab54818d2095f8afb1317
Rory Olsen <roris.solaris@gmail.com>
2013-04-12 13:49:38 -0600

    Adding reg name, tag, and not-tag to url params

--- simple_register_lookup.py
+++ simple_register_lookup.py
@@ -21,16 +21,28 @@ ALL_DEVICES_NAME = 'All Devices'
 @app.route("/")
 def show_ui():
     """Display the JavaScript client for viewing MODBUS map information."""
-    dropdown_options = modbus_maps.keys()
-    dropdown_options.insert(0, ALL_DEVICES_NAME)
+    device_options = modbus_maps.keys()
+    # TODO: add tags
+    device_options.insert(0, ALL_DEVICES_NAME)
     return flask.render_template(
         "simple_register_lookup.html",
-        device_names = dropdown_options
+        device_names = device_options
     )
 
 
-@app.route("/lookup/<device_name>.json")
-def lookup_device(device_name):
+@app.route("/lookup/<device_name>.json",
+    defaults={'tag': None, 'reg_name': None, 'not_tag': None})
+
+@app.route("/lookup/tag=<tag>",
+    defaults={'device_name': ALL_DEVICES_NAME, 'reg_name': None, 'not_tag': None})
+
+@app.route("/lookup/tag=<tag>&not_tag=<not_tag>",
+    defaults={'device_name': ALL_DEVICES_NAME, 'reg_name': None})
+
+@app.route("/lookup/reg_name=<reg_name>",
+    defaults={'device_name': ALL_DEVICES_NAME, 'tag': None, 'not_tag': None})
+
+def lookup_device(device_name, reg_name, tag, not_tag):
     """Render JSON formatted device MODBUS map."""
     modbus_map = []
     if device_name == ALL_DEVICES_NAME:
@@ -45,12 +57,41 @@ def lookup_device(device_name):
     if modbus_map == None:
         flask.abort(404)
 
+    # TODO: fix Horrible style here
+    # TODO: Set defaults to "None" instead of None
+    if tag:
+        temp = []
+        for entry in modbus_map:
+            for map_tag in entry['tags']:
+                if map_tag.find(unicode(tag)) != -1:
+                    temp.append(entry)
+
+        modbus_map = temp
+
+    if not_tag:
+        entries_to_remove = []
+        for entry in modbus_map:
+            for map_tag in entry['tags']:
+                if map_tag.find(unicode(not_tag)) != -1:
+                    entries_to_remove.append(entry)
+
+        for entry in entries_to_remove:
+            modbus_map.remove(entry)
+
+    if reg_name:
+        temp = []
+        for entry in modbus_map:
+            map_name = entry['name']
+            if map_name.find(unicode(reg_name)) != -1:
+                temp.append(entry)
+
+        modbus_map = temp
+
     modbus_map_serialized = serialize.serialize_device_modbus_map(modbus_map)
     response = flask.make_response(json.dumps(modbus_map_serialized))
     response.headers["X-XSS-Protection"] = "0"
     response.headers["Access-Control-Allow-Origin"] = "http://labjack.com"
     return response
-
 
 # Thanks http://www.peterbe.com/plog/uniqifiers-benchmark
 def uniques(seq, idfun=None):
