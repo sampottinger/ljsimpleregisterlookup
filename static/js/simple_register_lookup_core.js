4a8f79ccd93bbc06bc2c829c096dfb8ee6ad1e3c
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-10-22 15:16:33 -0600

    Updated target URL.

--- static/js/simple_register_lookup_core.js
+++ static/js/simple_register_lookup_core.js
@@ -1,6 +1,6 @@
 var LOOKUP = "/lookup.json?"
 
-var BASE_URL = "http://0.0.0.0:5000";
+var BASE_URL = "http://ljsimpleregisterlookup.herokuapp.com/";
 
 var DEPLOY_URL = BASE_URL + LOOKUP;
 var LOCAL_TEST_URL = LOOKUP;
