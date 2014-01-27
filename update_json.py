c92769e1d59b0a9c78e3e5a46d771443d55421ba
Sam P <sam@gleap.org>
2014-01-27 15:03:52 -0500

    Incremental JSON update.

--- update_json.py
+++ update_json.py
@@ -1,7 +1,8 @@
 import subprocess
 
 print 'Pulling from Git repository...'
-subprocess.check_call('git submodule foreach git pull origin master', shell=True)
+subprocess.check_call('git pull --recurse-submodules', shell=True)
+subprocess.check_call('git submodule update --recursive', shell=True)
 subprocess.check_call('git pull', shell=True)
 subprocess.call('git commit -a -m "Incremental JSON update."', shell=True)
 subprocess.call('git push', shell=True)
