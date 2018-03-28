fbc34373170f82bee77f4893541c34f6f74c74c2
calebneelCO <me@calebneel.com>
2018-03-28 14:50:35 -0600

    Add UnitTests

--- lj_error_scribe_test.py
+++ lj_error_scribe_test.py
@@ -40,12 +40,13 @@ EXPECTED_ERRORS = zip(EXPECTED_ERRORS)
 
 class LJErrorScribeTests(unittest.TestCase):
 
-    def test_find_error_range_from_errors_gets_all_errors(self):
-        errors = lj_error_scribe.find_error_range_from_errors(all, 0, EXPECTED_ERRORS)
-        self.assertEqual(EXPECTED_ERRORS, errors)
-    def test_find_error_range_from_errors_gets_one_error(self):
-      errors = lj_error_scribe.find_error_range_from_errors(0, 0, EXPECTED_ERRORS)
-      self.assertEqual(ONE_TEST_ERROR, errors)
+  def test_find_error_range_from_errors_gets_all_errors(self):
+      errors = lj_error_scribe.find_error_range_from_errors(0, 399, EXPECTED_ERRORS)
+      self.assertEqual(EXPECTED_ERRORS, errors)
+
+  def test_find_error_range_from_errors_gets_one_error(self):
+    errors = lj_error_scribe.find_error_range_from_errors(0, 0, EXPECTED_ERRORS)
+    self.assertEqual(ONE_TEST_ERROR, errors)
 
 if __name__ == "__main__":
   unittest.main()
