79b66baa9dcd537af2bd09c3a4af1d2d19651162
calebneelCO <me@calebneel.com>
2018-11-12 15:03:21 -0700

    Fix urls

--- lj_error_scribe.py
+++ lj_error_scribe.py
@@ -13,8 +13,23 @@ def format_errors(high, low):
     #sort errors based on high and low values
     output_dict = find_error_range_from_errors(low, high, raw_error)
     #send filtered json to tag_summary_template_error.html for rendering
+    for x in xrange(0,len(output_dict)):
+    	data = json.loads(json.dumps(output_dict[x][0]))
+    	if "description" in data:
+  			data["description"] = ljmmm.apply_anchors(data["description"])
+    	output_dict[x] = data
+    	#print output_dict
+    #	if "description" in data:
+  	#		data["description"] = ljmmm.apply_anchors(data["description"])
+    #	output_dict[x] = json.dumps(data)
+    	
+  			
+
+    #return output_dict
     return flask.render_template("tag_summary_template_error.html", tag =  output_dict)
 
 
 def find_error_range_from_errors(low, high, raw_error):
 	return [x for x in raw_error if x[0]['error'] >= low and x[0]['error'] <= high] 
+
+#print format_errors(1306,1306)
